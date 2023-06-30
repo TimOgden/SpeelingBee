ef4a7c51b16d6bb0001caaba569b425ac22cfb8a
Tim <togden1999@gmail.com>
2023-06-30 16:03:06 -0400

    Add sql scripts

---
+++ sql_scripts/delete_todays_words.sql
@@ -0,0 +1,2 @@
+delete from speelingbee.dailyword where date=CURRENT_DATE();
+delete from speelingbee.words where date=CURRENT_DATE();
---
+++ sql_scripts/reset_todays_words.sql
@@ -0,0 +1,1 @@
+update speelingbee.words set foundBy=null where date=CURRENT_DATE();
